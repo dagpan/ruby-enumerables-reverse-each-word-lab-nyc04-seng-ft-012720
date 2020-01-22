84ed348796063b7453baf52a22676d45f378d940
Panagiotis Ntagkinis <panagiotisntagkinis@gmail.com>
2020-01-22 01:21:02 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -12,11 +12,11 @@ def reverse_each_word(str = "Hello there, and how are you?")
   result_array = []
   array1 = str.split(" ")
   i = 0
-  array1.collect
-  
+  array1.collect do |word| 
+     result_array << "#{word.reverse}"
 #  array1.each do |word|
 #    result_array << "#{array1[i].reverse}"
-#    i += 1
+     i += 1
   end
   result_array.join(" ")
 end
